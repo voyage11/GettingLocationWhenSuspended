ee9e7f0decaf865f69332043432faa95669ec404
Yang <yang@electrodream.jp>
2015-05-27 10:52:20 +0900

    Add log list view

--- GettingLocationWhenSuspended/LocationViewController.h
+++ GettingLocationWhenSuspended/LocationViewController.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface LocationViewController : UIViewController
-
+{
+    NSMutableDictionary *savedProfile;
+    NSArray *locationArray;
+}
+@property (strong, nonatomic) IBOutlet UISegmentedControl *modeSeg;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 @end
